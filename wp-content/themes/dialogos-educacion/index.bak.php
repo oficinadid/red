08c410f46f6c216e51c84edc7adc2a138fbce781
Gustavo Astorga <gaptone@gmail.com>
2014-02-18 12:57:47 -0300

    DC Educacion / Home

---
+++ wp-content/themes/dialogos-educacion/index.bak.php
@@ -0,0 +1,140 @@
+        <?php get_header(); ?>
+
+
+        <div class="wrap main">
+
+            <div id="about">
+                <h2>Buscamos generar las bases ideológicas y político-prácticas que permita transformar Chile en una sociedad más justa, igualitaria y democrática.</h2>
+            </div>
+
+            <div id="participacion">
+                <h3>Participación de <strong>Diálogos Ciudadanos</strong></h3>
+                <h2>¿Pregunta o reflexión a contestar en Diálogos, responder en formulario ...?</h2>
+                <p>Está enfrentando y enfrentará en los próximos años. RED es un proyecto para pensar en el tipo de país y desarrollo que queremos y así transformar Chile en una sociedad más justa, igualitaria y democrática. Económicas que Chile está enfrentando y enfrentará en los próximos años.</p>
+                <form>
+                    <input id="nombre" type="text" placeholder="| Nombre">
+                    <input id="mail" type="text" placeholder="| Email">
+                    <textarea placeholder="| Respuesta"></textarea>
+                    <input id="submit" type="button" value="Danos tu Respuesta">
+                </form>
+            </div>
+
+            <div id="noticias" class="stream">
+
+                <h3>Noticias de <strong>Diálogos Ciudadanos</strong></h3>
+
+                <div class="post">
+                    <div class="top">
+                        <h2 class="title"><a href="#">Nueva versión disponible del Barómetro de Política y Equidad.</a></h2>
+                        <div class="meta">Diciembre 12, 2013 en categoría <a href="#">Papers</a> por <a href="#">Gonzalo Durán</a>.</div>
+                    </div>
+                    <div class="texto">
+                        <p>Chile está enfrentando y enfrentará en los próximos años. RED es un proyecto para pensar en el tipo de país y desarrollo que queremos y así transformar. RED es un proyecto para pensar en el tipo de país y desarrollo que queremos y así transformar Chile en una sociedad más justa, igualitaria y democrática.</p>
+                    </div>
+                </div>
+
+                <div class="cf"></div>
+
+                <div class="post">
+                    <div class="top">
+                        <h2 class="title"><a href="#">Nueva versión disponible del Barómetro de Política y Equidad.</a></h2>
+                        <div class="meta">Diciembre 12, 2013 en categoría <a href="#">Papers</a> por <a href="#">Gonzalo Durán</a>.</div>
+                    </div>
+                    <div class="texto">
+                        <p>Chile está enfrentando y enfrentará en los próximos años. RED es un proyecto para pensar en el tipo de país y desarrollo que queremos y así transformar. RED es un proyecto para pensar en el tipo de país y desarrollo que queremos y así transformar Chile en una sociedad más justa, igualitaria y democrática.</p>
+                    </div>
+                </div>
+
+                <div class="cf"></div>
+
+                <div class="post">
+                    <div class="top">
+                        <h2 class="title"><a href="#">Nueva versión disponible del Barómetro de Política y Equidad.</a></h2>
+                        <div class="meta">Diciembre 12, 2013 en categoría <a href="#">Papers</a> por <a href="#">Gonzalo Durán</a>.</div>
+                    </div>
+                    <div class="texto">
+                        <p>Chile está enfrentando y enfrentará en los próximos años. RED es un proyecto para pensar en el tipo de país y desarrollo que queremos y así transformar. RED es un proyecto para pensar en el tipo de país y desarrollo que queremos y así transformar Chile en una sociedad más justa, igualitaria y democrática.</p>
+                    </div>
+                </div>
+
+                <div class="cf"></div>
+
+                <a href="#" class="more">Ver más <strong>Noticias</strong> &#9662;</a>
+
+                <div class="cf"></div>
+
+            </div>
+
+        </div>
+
+        <div id="documentos">
+            <div class="wrap">
+                
+                <h3>Documentos de <strong>Diálogos Ciudadanos</strong></h3>
+
+                <div class="post pic">
+                    <a class="img" href="#"><img class="tn" src="<?php bloginfo('template_url'); ?>/img/dc-educacion-documentos1.jpg"></a>
+                    <div class="cf"></div>
+                    <div class="texto">
+                        <span class="fecha">Domingo 20 de Enero 2014</span>
+                        <h2><a href="#">Barómetro de Política y Equidad.</a></h2>
+                    </div>
+                    <span class="autor">Por <a href="#">Guillermo Durán</a></span>
+                </div>
+
+                <div class="post nopic">
+                    <div class="texto">
+                        <span class="fecha">Domingo 20 de Enero 2014</span>
+                        <h2><a href="#">Barómetro de Política y Equidad.</a></h2>
+                        <p>Chile está enfrentando y enfrentará en los próximos años. RED es un proyecto para pensar en el tipo de país y desarrollo que queremos y así transformar.</p>
+                    </div>
+                    <span class="autor">Por <a href="#">Guillermo Durán</a></span>
+                </div>
+
+                <div class="post nopic">
+                    <div class="texto">
+                        <span class="fecha">Domingo 20 de Enero 2014</span>
+                        <h2><a href="#">Barómetro de Política y Equidad.</a></h2>
+                        <p>Chile está enfrentando y enfrentará en los próximos años. RED es un proyecto para pensar en el tipo de país y desarrollo que queremos y así transformar.</p>
+                    </div>
+                    <span class="autor">Por <a href="#">Guillermo Durán</a></span>
+                </div>
+
+                <div class="post nopic">
+                    <div class="texto">
+                        <span class="fecha">Domingo 20 de Enero 2014</span>
+                        <h2><a href="#">Barómetro de Política y Equidad.</a></h2>
+                        <p>Chile está enfrentando y enfrentará en los próximos años. RED es un proyecto para pensar en el tipo de país y desarrollo que queremos y así transformar.</p>
+                    </div>
+                    <span class="autor">Por <a href="#">Guillermo Durán</a></span>
+                </div>
+
+                <div class="post pic">
+                    <a class="img" href="#"><img class="tn" src="<?php bloginfo('template_url'); ?>/img/dc-educacion-documentos1.jpg"></a>
+                    <div class="cf"></div>
+                    <div class="texto">
+                        <span class="fecha">Domingo 20 de Enero 2014</span>
+                        <h2><a href="#">Barómetro de Política y Equidad.</a></h2>
+                    </div>
+                    <span class="autor">Por <a href="#">Guillermo Durán</a></span>
+                </div>
+
+                <div class="post nopic">
+                    <div class="texto">
+                        <span class="fecha">Domingo 20 de Enero 2014</span>
+                        <h2><a href="#">Barómetro de Política y Equidad.</a></h2>
+                        <p>Chile está enfrentando y enfrentará en los próximos años. RED es un proyecto para pensar en el tipo de país y desarrollo que queremos y así transformar.</p>
+                    </div>
+                    <span class="autor">Por <a href="#">Guillermo Durán</a></span>
+                </div>
+
+                <div class="cf"></div>
+
+                <a href="#" class="more">Ver más <strong>Documentos</strong> &#9662;</a>
+
+                <div class="cf"></div>
+
+            </div>
+        </div>
+
+        <?php get_footer(); ?>
