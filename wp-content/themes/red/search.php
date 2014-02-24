0f298e134ba77637b02a00d195d27ac68bf8af58
david s <messthetic@gmail.com>
2014-02-24 01:48:51 -0300

    cambiamos a nuevo sistema de co-autores

--- wp-content/themes/red/search.php
+++ wp-content/themes/red/search.php
@@ -9,14 +9,17 @@
             <h3>Resultados de Búsqueda para <strong>"<?php echo $_GET['s'] ?>"</strong></h3>
 			<div class="infscr-content">
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-					<?php $colaboradores = get_field('colaboradores'); ?>
+					<?php
+	        			$fuente = get_field('fuente');
+	        			$coautores = get_coauthors();
+	        		?>
 					<div class="post infscr-item">
 		                <div class="top">
 		                    <h2 class="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 		                    <div class="meta"><?php the_time('F j, Y'); ?> por
-								<?php if (is_array($colaboradores)): ?>
-									<?php $c = 0; foreach ($colaboradores as $colaborador): ?>
-										<?php echo ($c != 0) ? ', ' : ' '; ?><a href="<?php get_permalink($colaborador); ?>"><?php echo get_the_title($colaborador); ?></a>
+								<?php if (is_array($coautores) && !$fuente): ?>
+									<?php $c = 0; foreach ($coautores as $coautor): ?>
+										<?php echo ($c != 0) ? ', ' : ' '; ?><a href="<?php get_permalink($coautor->ID); ?>"><?php echo get_the_title($coautor->ID); ?></a>
 										<?php $c++; endforeach ?>
 								<?php else: ?>
 									<?php the_field('fuente') ?>
